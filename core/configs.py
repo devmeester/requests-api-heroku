bd5359277c347aa9733908723af0c162aaca9cde
Kainã Bermudes <bermudes13@outlook.com>
2022-09-03 18:13:21 +0100

    Update configs.py

--- core/configs.py
+++ core/configs.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:bm1U7Qe6tBWqCOcmj4If@hardwaredb.czbomnjyhr9g.us-east-1.rds.amazonaws.com:5432/hardwaredb'
+    DB_URL: str = 'postgresql+asyncpg://postgres:9a0860B6@hardwaredb.cvezjiq03s8a.sa-east-1.rds.amazonaws.com:5432/hardwaredb'
 
     class Config:
         case_sensitive = True
